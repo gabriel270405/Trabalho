5dfb21f75514f80359ce0c65e1bb6945e670f99e
Gabriel <101403161+gabriel270405@users.noreply.github.com>
2022-03-30 11:06:26 -0300

    Add files via upload

---
+++ cadastrarcurso.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operacao.h"
+
+//executando o main do arquivo e abrindo o "cursogravado", 
+
+void main() {
+
+    gravar();
+}
---
+++ curso.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dados_curso
+{
+char nomedocurso[30];
+char descricao[50];
+char cargahoraria[10];
+char datadeinicio[10];
+char datadetermino[10];
+char preco[10];
+
+}curso;
+
